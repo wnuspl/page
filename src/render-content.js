99df486e044c28623e0f51bc033b959d289a07f5
wnuspl <nusplwill@gmail.com>
2026-02-17 21:25:33 -0500

    added in dev

--- src/render-content.js
+++ src/render-content.js
@@ -7,6 +7,9 @@ export function renderContent(json) {
             case "project":
                 item = createProjectElement(section);
                 break;
+            case "preview":
+                item = createPreviewElement(section);
+                break;
         }
 
         contentSection.appendChild(item);
@@ -17,7 +20,7 @@ export function renderContent(json) {
 
 
 const projectClasses = {
-    main: "flex-row",
+    main: "flex-row p-2",
     header: "text-lg font-semibold break-after-auto",
     description: "text-base bold",
     linkContainer: "flex-col",
@@ -53,6 +56,36 @@ function createProjectElement(project) {
     }
 
     div.appendChild(linkContainer);
+
+    return div;
+}
+
+
+const previewClasses = {
+    main: "bg-sky-50",
+    inDev: "italic"
+}
+
+function createPreviewElement(preview) {
+    const div = document.createElement("div");
+    div.classList.add(...projectClasses.main.split(" "), ...previewClasses.main.split(" "));
+
+    const header = document.createElement("h2");
+    header.classList.add(...projectClasses.header.split(" "));
+    header.innerText = preview.title;
+
+    const inDev = document.createElement("p");
+    inDev.classList.add(...projectClasses.description.split(" "), ...previewClasses.inDev.split(" "));
+    inDev.innerText = "in development";
+    
+    const description = document.createElement("p");
+    description.classList.add(...projectClasses.description.split(" "));
+    description.innerText = preview.description;
+
+    div.appendChild(header);
+    div.appendChild(inDev);
+    div.appendChild(description);
+
 
     return div;
 }
